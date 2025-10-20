6f507c24efc5652ce01e8b7cb3a11e4374eb523d
Kevin Adrian <kevin19305.ib@gmail.com>
2025-10-20 13:27:49 +0700

    kode ini Membuat API (PUT) di /api/users/:id untuk mengubah (update) data mahasiswa yang spesifik berdasarkan id-nya.

--- index.js
+++ index.js
@@ -60,3 +60,21 @@ app.post('/api/users', (req, res) => {
         }
     );
 });
+
+app.put('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const { nama, nim, kelas } = req.body; 
+    
+    db.query(
+        "UPDATE mahasiswa SET nama = ?, nim = ?, kelas = ? WHERE id = ?",
+        [nama, nim, kelas, userId],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ Message: "database error" });
+            }
+            res.json({ Message: "User updated successfully" });
+        }
+    );
+});
+
