c3958945f678a5b3fae7721fca1ddee47a34469c
Kevin Adrian <kevin19305.ib@gmail.com>
2025-10-20 13:05:01 +0700

    Kode ini mencoba menghubungkan aplikasi Anda ke database MySQL menggunakan konfigurasi db yang Anda buat sebelumnya.

--- index.js
+++ index.js
@@ -20,3 +20,11 @@ const db = mysql.createConnection({
   database: "biodata",
   port: 3308
 });
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:" + err.stack);
+    return;
+  }
+    console.log("Connection to database successful");
+});
